e3e5a1c1f09b61ad07e8d3c00e3e273cb2bf5d03
Odair Soares <odairsds@gmail.com>
2024-01-15 14:28:56 -0300

    exercício 7.3 implementado do meu jeito

--- capítulo_7/exercicio_7_3.py
+++ capítulo_7/exercicio_7_3.py
@@ -6,8 +6,17 @@ n = 0
 lista = []
 
 while n < len(string2):
-    if string2[n] not in string1:
-        lista.append(string2[n])
+    if string2[n] not in string1 and string2[n] != ' ':
+        if string2[n] not in lista:
+            lista.append(string2[n])
+    n += 1
+
+n = 0
+
+while n < len(string1):
+    if string1[n] not in string2  and string1[n] != ' ':
+        if string1[n] not in lista:
+            lista.append(string1[n])
     n += 1
 
 string3 = ''.join(lista)
