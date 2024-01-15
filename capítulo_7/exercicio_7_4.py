8013c36f514d2dd73152aad4d8a6bdf259c0e4dc
Odair Soares <odairsds@gmail.com>
2024-01-15 15:18:35 -0300

    exercício 7.4 implementado (pág. 143)

---
+++ capítulo_7/exercicio_7_4.py
@@ -0,0 +1,22 @@
+"""
+Exercício 7.4
+
+programa que ler uma string e imprime quantas vezes
+cada caractere dessa string aparece nela.
+
+"""
+
+string1 = input("informe uma string qualquer\npara saber quantas vezes\nseus caracteres se repetem: ")
+
+dicionario = {}
+
+for letra in string1:
+    if letra not in dicionario:
+        if letra != ' ':
+            dicionario[letra] = 1
+    else:
+        dicionario[letra] += 1
+
+for chave, valor in dicionario.items():
+    print(chave.upper() + ": %dx" % valor)
+    
