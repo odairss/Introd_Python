da5f97059e154f03000a2c8591c5596e72f9108b
Odair Soares <odairsds@gmail.com>
2024-02-10 11:15:10 -0300

    Solução para o exercício 8.7 da pág. 172.

---
+++ capítulo_8/exercicio_8_7.py
@@ -0,0 +1,42 @@
+"""
+Exercício 8.7 dá pág. 172:
+
+Este programa calcula o máximo divisor comum (MDC) entre dois números a e b, onde a > b.
+
+"""
+
+print("\nSOLUÇÃO INICIALMENTE ARRANJADA POR MIM:\n")
+
+a = int(input("Informe o valor de A: "))
+b = int(input("Informe o valor de B, onde B < A: "))
+
+num = 1
+divisor = 1
+n = 1
+
+def mdc(n):
+    global divisor
+    if n <= b:
+        if a%n==0 and b%n==0:
+            divisor = n
+        mdc(n+1)
+    return divisor
+
+print(f"O máximo divisor comum entre {a} e {b} é: {mdc(num)}")
+
+
+print("\n\nSOLUÇÃO ARRANJADA APÓS CONSULTAR O CHAT GPT:\n")
+
+a = int(input("Informe o valor de A: "))
+b = int(input("Informe o valor de B, onde B < A: "))
+
+def mdc_gpt(a,b):
+    resto = a%b
+    if resto > 0:
+        a = b
+        b = resto
+        mdc_gpt(a,b)
+    return resto
+        
+
+print(f"O máximo divisor comum entre {a} e {b} é: {mdc_gpt(a,b)}")
