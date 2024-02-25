6baafc7dae03c6b6ccea5947fb8db3ee40c68930
Odair Soares <odairsds@gmail.com>
2024-02-25 18:32:38 -0300

    Solução para o exercício 9.7 da pág. 194

--- capítulo_9/exercicio_9_7.py
+++ capítulo_9/exercicio_9_7.py
@@ -6,18 +6,31 @@ pode ter no máximo 60 linhas e no final de cada página tem o número da pág.
 pág. 194
 
 """
-arquivo_original = open('texto_qualquer.txt')
-nun_linha = 1
-num_pagina = 1
-tam = 0
-sobra = ''
-for line in arquivo_original.readlines():
-    line = line+sobra
-    if len(line) <= 76:
-        print(str(nun_linha)+' '+line)
+lorem_ipsum = open('lorem_ipsum.txt')
+lorem_ipsum_paginado = open('lorem_ipsum_paginado.txt','w')
+NUM_LINHA = 1
+NUM_PAGINA = 1
+TAM = 0
+SOBRA = ''
+for line in lorem_ipsum.readlines():
+    if NUM_LINHA > 60:
+        NUM_LINHA = 0
+        lorem_ipsum_paginado.write('\n')
+        lorem_ipsum_paginado.write(f'{NUM_PAGINA}\n\n'.rjust(81,'.'))
+        NUM_PAGINA += 1
     else:
-        tam = len(line)
-        linha = line.slice[:76]
-        print(str(nun_linha)+' '+linha)
-        sobra = line.slice[76:tam]
-    num_linha += 1
+        linha_e_sobra = SOBRA+line
+        linha_e_sobra = linha_e_sobra.strip()
+        if len(linha_e_sobra) == 0:
+            lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM_LINHA))+'| '+linha_e_sobra+'\n')
+        elif len(linha_e_sobra) > 0 and len(linha_e_sobra) <= 76:
+            lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM_LINHA))+'| '+linha_e_sobra+'\n')
+        else:
+            linha = linha_e_sobra[:76]
+            lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM_LINHA))+'| '+linha+'\n')
+            TAM = len(linha_e_sobra)
+            SOBRA = linha_e_sobra[76:TAM]
+    NUM_LINHA += 1
+
+lorem_ipsum.close()
+lorem_ipsum_paginado.close()
