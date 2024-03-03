9cadb6c8fbcce41428f410b9b7577438caeaf35f
Odair Soares <odairsds@gmail.com>
2024-03-03 17:44:32 -0300

    Atualização no código do programa referente ao exercício 9.7 da pág. 194

--- capítulo_9/exercicio_9_7.py
+++ capítulo_9/exercicio_9_7.py
@@ -6,31 +6,50 @@ pode ter no máximo 60 linhas e no final de cada página tem o número da pág.
 pág. 194
 
 """
+
+NUM_PAGINA = 1
+TAMANHO = 0
+NUM = 1
+
 lorem_ipsum = open('lorem_ipsum.txt')
 lorem_ipsum_paginado = open('lorem_ipsum_paginado.txt','w')
-NUM_LINHA = 1
-NUM_PAGINA = 1
-TAM = 0
-SOBRA = ''
-for line in lorem_ipsum.readlines():
-    if NUM_LINHA > 60:
-        NUM_LINHA = 0
-        lorem_ipsum_paginado.write('\n')
-        lorem_ipsum_paginado.write(f'{NUM_PAGINA}\n\n'.rjust(81,'.'))
-        NUM_PAGINA += 1
-    else:
-        linha_e_sobra = SOBRA+line
-        linha_e_sobra = linha_e_sobra.strip()
-        if len(linha_e_sobra) == 0:
-            lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM_LINHA))+'| '+linha_e_sobra+'\n')
-        elif len(linha_e_sobra) > 0 and len(linha_e_sobra) <= 76:
-            lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM_LINHA))+'| '+linha_e_sobra+'\n')
-        else:
-            linha = linha_e_sobra[:76]
-            lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM_LINHA))+'| '+linha+'\n')
-            TAM = len(linha_e_sobra)
-            SOBRA = linha_e_sobra[76:TAM]
-    NUM_LINHA += 1
+
+def main():
+    global NUM, TAMANHO, NUM_PAGINA
+    for line in lorem_ipsum.readlines():
+        TAMANHO = len(line)
+        while len(line) > 76:
+            if NUM > 60:
+                ultima_linha = '\n'
+                ultima_linha += f'{NUM_PAGINA}\n\n'.rjust(81,'.')
+                lorem_ipsum_paginado.write(ultima_linha)
+                NUM_PAGINA += 1
+                NUM = 1
+            else:
+                linha = line[:76]
+                line = line[76:TAMANHO]
+                TAMANHO = len(line)
+                lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM))+'| '+linha+'\n')
+                NUM += 1      
+            
+        if len(line) <= 76:
+            if NUM > 60:
+                ultima_linha = '\n'
+                ultima_linha += f'{NUM_PAGINA}\n\n'.rjust(81,'.')
+                lorem_ipsum_paginado.write(ultima_linha)
+                NUM_PAGINA += 1
+                NUM = 1
+                lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM))+'| '+line)
+                NUM += 1
+            else:
+                lorem_ipsum_paginado.write('{0:>2}'.format(str(NUM))+'| '+line)
+                NUM += 1
+    ultima_linha = '\n'
+    ultima_linha += f'{NUM_PAGINA}\n\n'.rjust(81,'.')
+    lorem_ipsum_paginado.write(ultima_linha)
+    
+if __name__ == '__main__':
+    main()
 
 lorem_ipsum.close()
 lorem_ipsum_paginado.close()
