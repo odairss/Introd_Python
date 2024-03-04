553ed57e9414669625d808560adfb7e7ee5f84ef
Odair Soares <odairsds@gmail.com>
2024-03-04 17:56:10 -0300

    Atualização no código do programa 9.7 da pág. 194 para adicionar o nome do arquivo lido no final de cada página.

--- capítulo_9/exercicio_9_7.py
+++ capítulo_9/exercicio_9_7.py
@@ -14,10 +14,11 @@ TEXTO = ''
 lorem_ipsum = open('lorem_ipsum.txt')
 lorem_ipsum_paginado = open('lorem_ipsum_paginado.txt','w')
 
-def numerar_pagina():
+def numerar_pagina(nome_original):
     global NUM_PAGINA, NUM
     ultima_linha = '\n'
-    ultima_linha += f'{NUM_PAGINA}\n\n'.rjust(81,'.')
+    ultima_linha += f'{NUM_PAGINA}\n'.rjust(81,'.')
+    ultima_linha += nome_original + '\n\n'
     NUM_PAGINA += 1
     NUM = 1
     return ultima_linha
@@ -31,23 +32,23 @@ def numerar_linha(ln):
     NUM += 1
     return [tx,ln]
 
-def se_linha_grande(linha_g):
+def se_linha_grande(linha_g, nome):
     global TEXTO
     while len(linha_g) > 76:
         if NUM > 60:
-            TEXTO += numerar_pagina()
+            TEXTO += numerar_pagina(nome)
         else:
             tx_sbr = numerar_linha(linha_g)
             linha_g = tx_sbr[1]
             TEXTO += tx_sbr[0]
     return [TEXTO, linha_g]
 
-def se_linha_pequena(linha_p):
+def se_linha_pequena(linha_p, nome):
     global NUM
     tx = ''
     if len(linha_p) <= 76:
         if NUM > 60:
-            tx = numerar_pagina()
+            tx = numerar_pagina(nome)
             tx += '{0:>2}'.format(str(NUM))+'| '+linha_p
             NUM += 1
         else:
@@ -59,13 +60,14 @@ def main():
     global TEXTO
     for line in lorem_ipsum.readlines():
         TEXTO = ''
-        result = se_linha_grande(line)
+        result = se_linha_grande(line, lorem_ipsum.name)
         lorem_ipsum_paginado.write(result[0])
         line = result[1]
-        lorem_ipsum_paginado.write(se_linha_pequena(line))
+        lorem_ipsum_paginado.write(se_linha_pequena(line, lorem_ipsum.name))
                 
     ultima_linha = '\n'
-    ultima_linha += f'{NUM_PAGINA}\n\n'.rjust(81,'.')
+    ultima_linha += f'{NUM_PAGINA}\n'.rjust(81,'.')
+    ultima_linha += lorem_ipsum.name + '\n\n'
     lorem_ipsum_paginado.write(ultima_linha)
     
 if __name__ == '__main__':
